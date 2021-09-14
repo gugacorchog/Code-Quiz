aae2686cc45c76a04b620ab2950a7260fe7d2f17
Gustavo Corchog <gugacorchog@gmail.com>
2021-09-15 00:02:37 +1000

    Js and Css update

--- logic.js
+++ logic.js
@@ -1,32 +1,34 @@
 var timeDisplay = document.getElementById("timeDisplay")
-var timer = document.getElementById ("timer")
-var startButton = document.getElementById ("start-btn")
-var startScreen = document.getElementById ("start-screen")
-var questions = document.getElementById ("questions")
-var questionTitle = document.getElementById ("question-title")
-var questionContainer = document.getElementById ("questions-container")
-var questionDiv = document.getElementById ("question-title")
-var choices = document.getElementById ("choices")
-var answerBtn1 = document.getElementById ('answer1')
-var answerBtn2 = document.getElementById ('answer2')
-var answerBtn3 = document.getElementById ('answer3')
-var answerBtn4 = document.getElementById ('answer4')
-var feedback = document.getElementById ('feedBack') 
+var timer = document.getElementById("timer")
+var startButton = document.getElementById("start-btn")
+var startScreen = document.getElementById("start-screen")
+var questions = document.getElementById("questions")
+var questionTitle = document.getElementById("question-title")
+var questionContainer = document.getElementById("questions-container")
+var questionDiv = document.getElementById("question-title")
+var choices = document.getElementById("choices")
+var answerBtn1 = document.getElementById('answer1')
+var answerBtn2 = document.getElementById('answer2')
+var answerBtn3 = document.getElementById('answer3')
+var answerBtn4 = document.getElementById('answer4')
+var feedback = document.getElementById('feedBack') 
 var qNum = 0
 var timeLeft = 0
 var quizTime = 0
 var score = 0
+
 
 // this function is to check if current page is highscore page
 if (title.innerHTML === "Highscores") {
     renderTable();
 }
 
+
 // set initial timer value and fire off two functions
-function quizStart() {
+function initiate() {
     timeLeft = 75
     startTimer();
-    initQ();
+   
 }
 //  function changes timer display every tick (second)
 function startTimer() {
@@ -43,7 +45,7 @@ function tick() {
         clearInterval(quizTime)
         quizOver();
     }
-    return;
+         return;
 }
 
 
@@ -51,17 +53,18 @@ function quiz() {
     if (qNum >= questionsArray.length) {
     quizOver();
 }
-else {
-    questionTitle.innerHTML = (questionsArray[qNum].title)
-    answerBtn1.innerHTML = (questionsArray[qNum].choices[1])
-    answerBtn2.innerHTML = (questionsArray[qNum].choices[2])
-    answerBtn3.innerHTML = (questionsArray[qNum].choices[3])
-    answerBtn4.innerHTML = (questionsArray[qNum].choices[4])
-}}
+    else {
+        questionTitle.textContent = questionsArray[qNum].title
+        answerBtn1.textContent = questionsArray[qNum].choices[0]
+        answerBtn2.textContent = questionsArray[qNum].choices[1]
+        answerBtn3.textContent = questionsArray[qNum].choices[2]
+        answerBtn4.textContent = questionsArray[qNum].choices[3]
+        
+    }
+}
 
 //  function checks whether or not answer is the correct one
 function answerCheck(btnId) {
-    console.log(btnId)
     if ((document.getElementById(btnId).innerHTML) === (questionsArray[qNum].answer)) {
         rightAnswer();
         qNum++
@@ -85,13 +88,44 @@ function wrongAnswer() {
     setTimeout(function() {feedback.innerHTML = ("");}, 800)
 }
 
-startButton.addEventListener('click', startQuiz);
+// Game is over functions
 
-function startQuiz() {
-    console.log('startQuiz');
+function quizOver(){
+    document.querySelectorAll(".quiz").forEach(quiz => {quiz.style.display = "none"})
+    var content = document.getElementById('theContent')
+    var done = document.getElementById('done')
+    var submit = document.getElementById('submit')
+
+    timer.innerHTML = (0)
+// Added text and button to submit afert finish game 
+    content.insertAdjacentHTML('afterbegin', '<h1 id="done">All Done!</h1> <button id="submit" class="btn btn-danger">Submit</button> <input id="userScore"> - Enter Initials</input>');
+
+    var done = document.getElementById('done')
+    done.insertAdjacentHTML('afterend', '<p id="finalScore">Your final score is ' + score + '</p>');
+// Function to add to local Storage
+    var submit = document.getElementById('submit')
+    submit.addEventListener("click", function(){
+    var value = document.getElementById('userScore').value;
+    localStorage.setItem(value, score)
+    window.location.href = "highscore.html"    
+
+    });
+    clearInterval(quizTime)
+}
+
+// start Quiz button and initiate questions function
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {    
+    
     questions.classList.remove('hide');
     startScreen.setAttribute("style", "display:none;");
-   
+    questionTitle.textContent = questionsArray[qNum].title
+    answerBtn1.textContent = questionsArray[qNum].choices[0]
+    answerBtn2.textContent = questionsArray[qNum].choices[1]
+    answerBtn3.textContent = questionsArray[qNum].choices[2]
+    answerBtn4.textContent = questionsArray[qNum].choices[3]
+    initiate()
 }
 
 
